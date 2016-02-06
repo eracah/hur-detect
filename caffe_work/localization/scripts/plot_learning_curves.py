391dd93207d94dd0512e7cdf3eee0fd31cdaf1c9
Evan Racah <ejracah@gmail.com>
2016-02-05 17:41:14 -0800

    Implemented plottng learning curver and plotting prob maps

---
+++ caffe_work/localization/scripts/plot_learning_curves.py
@@ -0,0 +1,42 @@
+__author__ = 'racah'
+import h5py
+import numpy
+import sys
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib import patches
+import os
+import numpy as np
+from pyspark import SparkContext
+
+caffe_output_file = sys.argv[1]
+plot_dir = sys.argv[2]
+if not os.path.exists(plot_dir):
+    os.mkdir(plot_dir)
+sc = SparkContext()
+
+lines = sc.textFile(caffe_output_file)
+lines.cache()
+loss = lines.filter(lambda x: ', loss =' in x )\
+    .map(lambda y: y.split(']')[-1].split())\
+    .map(lambda z: (int(z[1][:-1]), float(z[-1])))
+losses = np.asarray(loss.collect())
+
+accuracies = lines.filter(lambda x: 'seg-accuracy =' in x ).\
+    map(lambda y: float(y.split('= ')[-1]))
+
+acc = np.asarray(accuracies.collect())
+plt.figure(1)
+plt.plot(losses[:,0], losses[:,1])
+plt.xlabel('iteration')
+plt.ylabel('loss')
+plt.savefig(plot_dir + '/loss_curve.jpg')
+
+plt.clf()
+plt.figure(1)
+plt.plot(np.arange(acc.shape[0]),acc)
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.savefig(plot_dir + '/accuracy_curve.jpg')
+
--- caffe_work/localization/start_train-fewer.sh
+++ caffe_work/localization/start_train-fewer.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -l
+run_dir=`./scripts/create_run_dir.py`
+caffe train -solver network/deconv-net/solver_fewer.prototxt > $run_dir/train_fewer.txt
+./scripts/plot_learning_curves.py $run_dir/train_fewer.txt $run_dir/plots
 
-caffe train -solver network/deconv-net/solver_fewer.prototxt
+
